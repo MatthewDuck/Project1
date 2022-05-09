afb2e63eac1ca69e4d7e4b6af4e9a4c7eef1a6fa
Matthew Duck <MatthewDDuck@googlemail.com>
2022-05-09 16:38:07 +0100

    Created method to create new order

---
+++ src/main/java/com/qa/ims/controller/OrderController.java
@@ -0,0 +1,53 @@
+package com.qa.ims.controller;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.dao.OrderDAO;
+import com.qa.ims.persistence.domain.Customer;
+import com.qa.ims.persistence.domain.Order;
+import com.qa.ims.utils.Utils;
+
+public class OrderController implements CrudController<Order> {
+	
+	public static final Logger LOGGER = LogManager.getLogger();
+	
+	private OrderDAO orderDAO;
+	private Utils utils;
+	
+	public OrderController(OrderDAO orderDAO, Utils utils) {
+		this.orderDAO = orderDAO;
+		this.utils = utils;
+	}
+
+	@Override
+	public List<Order> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order create() {
+		LOGGER.info("Please enter a customer id");
+		Long customerId = utils.getLong();
+		Order order = orderDAO.create(new Order(customerId));
+		LOGGER.info("Order created");
+		return order;
+	}
+
+	@Override
+	public Order update() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+
+}
